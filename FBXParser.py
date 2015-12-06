9b54885c071e1e09da854e86b38a1dc48e80e7c0
Adam Joyce <adamjoyce@outlook.com>
2015-12-06 12:39:58 +0000

    Basic svg file generation - issues with rotation and initial placement location

--- FBXParser.py
+++ FBXParser.py
@@ -12,7 +12,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.wfile.write("<body><p>test</p></body>")
         s.wfile.write("<p>You accessed path: %s</p>" % s.path)
         s.wfile.write("</body></html>")
-
+'''
 def display(node, indent):
     if not node: return
 
@@ -23,28 +23,66 @@ def display(node, indent):
 
         if attr_type == FbxCommon.FbxNodeAttribute.eMesh:
             print(child)
-            #print(child.GetMesh().GetPolygonVertexCount())
-
+            
         display(child, indent + "  ")
+'''
+def generate_svg(fbx_path):
+    sdk_manager, scene = FbxCommon.InitializeSdkObjects()
 
-def get_vertices(node):
-    if not node: return
+    if not FbxCommon.LoadScene(sdk_manager, scene, fbx_path):
+        print("Error in LoadScene")
+    
+    node = scene.GetRootNode()
 
     for i in range(node.GetChildCount()):
-        print("Break")
         child = node.GetChild(i)
-        attr_type = child.GetNodeAttribute().GetAttributeType()
+        mesh = child.GetMesh()
 
-        if attr_type == FbxCommon.FbxNodeAttribute.eMesh:
-            vertices = child.GetMesh().GetControlPoints()
+        edges = get_edges(mesh)
+        edges = get_control_points(mesh, edges)
 
-            for i in range(len(vertices)):
-                print(vertices[i])
+    svg_content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+    svg_content += "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"400\" height=\"360\">\n"
+    svg_content += "<g fill=\"none\" stroke=\"#000\" stroke-width=\"3\">\n"
+    svg_content += generate_svg_path(edges) + "\n"
+    svg_content += "</g>\n" + "</svg>"
+    print(svg_content)
 
-sdk_manager, scene = FbxCommon.InitializeSdkObjects()
+    svg_file = open("box_wire.svg", "w")
+    svg_file.write(svg_content)
+    svg_file.close()
 
-if not FbxCommon.LoadScene(sdk_manager, scene, "cube.fbx"):
-    print("Error in LoadScene")
+def get_edges(mesh):
+    edges = []
+        
+    for j in range(mesh.GetMeshEdgeCount()):
+        edges.append(mesh.GetMeshEdgeVertices(j))
 
-#display(scene.GetRootNode(), "")
-get_vertices(scene.GetRootNode())
+    return edges
+
+def get_control_points(mesh, edge_array):
+    control_points = []
+
+    for i in range(len(edge_array)):
+        temp = []
+
+        for j in range(len(edge_array[i])):
+            temp.append(mesh.GetControlPointAt(edge_array[i][j]))
+
+        control_points.append(temp)
+
+    return control_points
+
+def generate_svg_path(edge_array):
+    svg_path = "<path d=\""
+
+    for i in range(len(edge_array)):
+        temp = "M" + str(edge_array[i][0][0]) + "," + str(edge_array[i][0][1]) + " "
+        temp += "L" + str(edge_array[i][1][0]) + "," + str(edge_array[i][1][1]) + " "
+
+        svg_path += temp
+
+    svg_path += "\"/>"
+    return svg_path
+
+generate_svg("cube.fbx")
